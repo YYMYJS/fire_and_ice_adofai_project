c30fa821d7e80e19a65566ae49469704b51e7c52
YYMYJS <151927762+YYMYJS@users.noreply.github.com>
2026-06-09 15:59:54 +0800

    Add path inspection script

---
+++ scripts/show_paths.py
@@ -0,0 +1,7 @@
+from fire_and_ice_adofai.paths import project_root, external_root
+from fire_and_ice_adofai.paths import llamafactory_path, mug_diffusion_path
+
+print("root=", project_root())
+print("external=", external_root())
+print("llamafactory=", llamafactory_path())
+print("mug_diffusion=", mug_diffusion_path())
